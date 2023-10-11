fb6bf089c3f8732ee7abd2f45f1e5553bbfd32dc
verica-kar <vericakaranakova@gmail.com>
2023-10-11 16:42:27 -0500

    done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init(){
+//add your javascrip between these two lines of code
+  var button = document.getElementById('entrybutton');
+
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Verica Karanakova: " + textbox.value);
+  }
+
+  button.addEventListener('click', showMeText);
+  }
+window.addEventListener('load', init);
